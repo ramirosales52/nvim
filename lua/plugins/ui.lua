2adfc43fd751a7e43edbf73cff86d4b06ae22cfa
Ramiro Rosales <ramirosales52@gmail.com>
2025-03-18 17:32:48 -0300

    nvim init

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,25 @@
+return {
+  "nvimdev/dashboard-nvim",
+  lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+  opts = function()
+    local logo = [[
+
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+                                                                       
+]]
+    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    local opts = {
+      config = {
+        header = vim.split(logo, "\n"),
+      },
+    }
+    return opts
+  end,
+}
